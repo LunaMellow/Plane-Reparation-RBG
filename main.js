f57c9e5fc9bba435b67e30d3cebc9de8a881aac3
LunaMellow <nore0404@gmail.com>
2021-12-07 15:45:07 +0100

    Disclaimer

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+  
+    Mellowie 2021 - All Rights Reserved 
+    <!DISCLAIMER!>
+    This is a school project and is not to be copied
+    for commercial or redistribution. You may learn,
+    edit for your personal use or admire my work as
+    much as you possibly want, but no copying ;)
+    - Mellowie <3
+*/
+
